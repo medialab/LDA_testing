0ae9351989e01d6232e25b7a0584b75cfd04f705
Diégo Antolinos-Basso <diego.antolinosbasso@sciencespo.fr>
2016-07-27 13:47:16 +0200

    Comments on parameters of lda.gibbs function in r script

--- r/lda_tm.R
+++ r/lda_tm.R
@@ -18,7 +18,7 @@ files <- lapply(files, paste, collapse = " ")
 # Builds a VCorpus with tm
 corpus <- Corpus(VectorSource(files))
 
-# Apply various treatments on corpus
+# Apply various treatments on corpus (explicit)
 corpus <- tm_map(corpus, removePunctuation)
 corpus <- tm_map(corpus, removeNumbers)
 corpus <- tm_map(corpus, tolower)
@@ -26,7 +26,7 @@ corpus <- tm_map(corpus, removeWords, stopwords("english"))
 corpus <- tm_map(corpus, removeWords, stopwords("french"))
 corpus <- tm_map(corpus, removeWords, stopwords("german"))
 corpus <- tm_map(corpus, stripWhitespace)
-# paperCorp <- tm_map(paperCorp, stemDocument)
+# paperCorp <- tm_map(paperCorp, stemDocument) # optional stemming
 
 # Retransforms the modified content of the Corpus into a simple vector of texts
 files <- c(corpus$content)
@@ -39,8 +39,21 @@ documents <- lexical$documents
 vocab <- lexical$vocab
 
 # Builds the LDA model from the documents and vocab (10 topics and 100 runs)
-modelda <- lda.collapsed.gibbs.sampler(documents, 10, vocab, 100, 0.1, 0.1)
+modelda <- lda.collapsed.gibbs.sampler(documents, 10, vocab, 500, 0.1, 0.1)
 
 # Prints out the top 10 words for each 10 topics
 print(top.topic.words(modelda$topics))
 
+###################
+# Useful comments #
+###################
+
+# The lda.collapsed.gibbs.sampler has the following arguments in this script
+# documents : A list whose length is equal to the number of documents, each element of documents is an integer matrix with two rows.  Each column of documents[[i]] (i.e., document i ) represents a word occurring in the document.
+# 10 (2nd positional arg) : the desired number of topics in the model
+# vocab : A character vector specifying the vocabulary words associated with the word indices used in documents.
+# 500 (4th positional arg) : number of run the model should run (no control for convergence implemented so this params depends on the time you have to run the script, leave it between 500 and 1000)
+# 0.1 (5th positional arg) : alpha, Dirichlet parameter for distribution over topics
+# 0.1 (6th positional arg) : eta, Dirichlet parameter for distribution over words
+# These last two should be between 0 and 1, default is 0.1. They're the "priors" and control distribution of probability
+# A bit of litterature concerning that : http://papers.nips.cc/paper/3854-rethinking-lda-why-priors-matter
