cd32c8772aa952e908b223b15193a9fc7d215031
Diégo Antolinos-Basso <diego.antolinosbasso@sciencespo.fr>
2016-07-27 13:31:39 +0200

    Comments on parameters of vectorizer and lda function

--- python/lda_numpy_sklearn.py
+++ python/lda_numpy_sklearn.py
@@ -47,4 +47,24 @@ for i, topic_dist in enumerate(topic_word):
 #     topic_most_pr = doc_topic[n].argmax()
 #     print("doc: {} topic: {}".format(n, topic_most_pr))
 
+###################
+# Useful comments #
+###################
+
+# CountVectorizer : http://scikit-learn.org/stable/modules/generated/sklearn.feature_extraction.text.CountVectorizer.html
+# the method has multiple parameters that can be exploited, here's a unexhaustive list of cool stuff
+# - takes filename, file object, or content (string) as input
+# - tokenizer can be overridden
+# - can build ngrams instead of words (as terms in the document-term-matrix)
+# - max_df and min_df params can be used to exclude words with document frequency higher or lower than thresholds
+
+# lda.LDA() parameters :
+# n_topics = number of topics
+# n_iter = number of iteration the model should run (no control for convergence of EM algo here so leave it between 500 and 1000 depending on the time you have)
+# random_state = I have no idea what that is honestly
+# + optional parameters
+# alpha = Dirichlet parameter for distribution over topics
+# eta = Dirichlet parameter for distribution over words
+# These two should be between 0 and 1, default is 0.1. They're the "priors" and control distribution of probability
+# A bit of litterature concerning that : http://papers.nips.cc/paper/3854-rethinking-lda-why-priors-matter
 
